0c9c00984ff1676621e28675deb0d4b621272114
vishalmudgal4996 <vishalmudgal4996@gmail.com>
2020-09-18 14:39:07 +0530

    login card designed

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Controller from "./screens/Controller";
+import "typeface-roboto";
 
 ReactDOM.render(<Controller />, document.getElementById("root"));
--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -1,6 +1,28 @@
 import React, { Component } from "react";
 import "./Login.css";
 import Header from "../../common/header/Header";
+import { withStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import FormControl from "@material-ui/core/FormControl";
+import Typography from "@material-ui/core/Typography";
+import InputLabel from "@material-ui/core/InputLabel";
+import Input from "@material-ui/core/Input";
+import Button from "@material-ui/core/Button";
+
+const styles = (theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 360,
+    maxWidth: 360,
+  },
+  title: {
+    textAlign: "left",
+  },
+  cardContent: {
+    textAlign: "center",
+  },
+});
 
 class Login extends Component {
   constructor() {
@@ -8,12 +30,42 @@ class Login extends Component {
     this.state = {};
   }
   render() {
+    const { classes } = this.props;
     return (
       <div>
         <Header />
+        <div className="flex-container">
+          <div className="login-card">
+            <Card>
+              <CardContent className={classes.cardContent}>
+                <FormControl className={classes.formControl}>
+                  <Typography color="textPrimary" className="card-title">
+                    LOGIN
+                  </Typography>
+                </FormControl>
+                <FormControl required className={classes.formControl}>
+                  <InputLabel htmlFor="username">Username</InputLabel>
+                  <Input id="username" onChange={this.usernameChangeHandler} />
+                </FormControl>
+                <FormControl required className={classes.formControl}>
+                  <InputLabel htmlFor="password">Password</InputLabel>
+                  <Input id="password" onChange={this.passwordChangeHandler} />
+                </FormControl>
+                <br />
+                <br />
+                <FormControl className={classes.formControl}>
+                  <Button variant="contained" color="primary" id="login-btn">
+                    LOGIN
+                  </Button>
+                </FormControl>
+                <br />
+              </CardContent>
+            </Card>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-export default Login;
+export default withStyles(styles)(Login);
