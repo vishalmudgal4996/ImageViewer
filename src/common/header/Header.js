79d18b80a8fe7bb5095be0883804947548200110
vishalmudgal4996 <vishalmudgal4996@gmail.com>
2020-09-19 21:50:05 +0530

    search box designed

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,5 +1,45 @@
 import React, { Component } from "react";
 import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import InputBase from "@material-ui/core/InputBase";
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = (theme) => ({
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: "#c0c0c0",
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: "300px",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(3),
+      width: "300px",
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
+    },
+  },
+});
 
 class Header extends Component {
   constructor() {
@@ -7,11 +47,30 @@ class Header extends Component {
     this.state = {};
   }
   render() {
+    const { classes } = this.props;
     return (
       <div>
         <header className="app-header">
-          <div className="app-logo">
-            <span className="app-logo-text">Image Viewer</span>
+          <div className="flex-container-header">
+            <div className="app-logo">
+              <span className="app-logo-text">Image Viewer</span>
+            </div>
+            <div className="homepage-header">
+              <div className={classes.search}>
+                <div className={classes.searchIcon}>
+                  <SearchIcon />
+                </div>
+                <InputBase
+                  placeholder="Search…"
+                  classes={{
+                    root: classes.inputRoot,
+                    input: classes.inputInput,
+                  }}
+                  inputProps={{ "aria-label": "search" }}
+                />
+              </div>
+              <div className="avatar">avatar</div>
+            </div>
           </div>
         </header>
       </div>
@@ -19,4 +78,4 @@ class Header extends Component {
   }
 }
 
-export default Header;
+export default withStyles(styles)(Header);
--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -12,64 +12,64 @@ class Home extends Component {
     };
   }
 
-  UNSAFE_componentWillMount() {
-    //xhr for get posts
+  // UNSAFE_componentWillMount() {
+  //   //xhr for get posts
 
-    let data = null;
-    let xhr = new XMLHttpRequest();
-    let that = this;
-    let access_token = this.state.access_token;
-    let url =
-      "https://graph.instagram.com/me/media?fields=id,caption&access_token=" +
-      access_token;
-    let mediaId = [];
+  //   let data = null;
+  //   let xhr = new XMLHttpRequest();
+  //   let that = this;
+  //   let access_token = this.state.access_token;
+  //   let url =
+  //     "https://graph.instagram.com/me/media?fields=id,caption&access_token=" +
+  //     access_token;
+  //   let mediaId = [];
 
-    xhr.addEventListener("readystatechange", function() {
-      if (this.readyState === 4) {
-        that.setState({ postsList: JSON.parse(this.responseText).data });
-        that.state.postsList.forEach((post) => {
-          mediaId.push(post.id);
-        });
-      }
-    });
+  //   xhr.addEventListener("readystatechange", function() {
+  //     if (this.readyState === 4) {
+  //       that.setState({ postsList: JSON.parse(this.responseText).data });
+  //       that.state.postsList.forEach((post) => {
+  //         mediaId.push(post.id);
+  //       });
+  //     }
+  //   });
 
-    xhr.open("GET", url);
-    xhr.setRequestHeader("Cache-Control", "no-cache");
-    xhr.send(data);
+  //   xhr.open("GET", url);
+  //   xhr.setRequestHeader("Cache-Control", "no-cache");
+  //   xhr.send(data);
 
-    //xhr for media data
+  //   //xhr for media data
 
-    let urlMedia;
-    let xhrMedia = new XMLHttpRequest();
-    let dataMedia = null;
+  //   let urlMedia;
+  //   let xhrMedia = new XMLHttpRequest();
+  //   let dataMedia = null;
 
-    setTimeout(function() {
-      if (mediaId.size !== 0) {
-        mediaId.forEach((id) => {
-          // console.log(id);
-          urlMedia =
-            "https://graph.instagram.com/" +
-            id +
-            "?fields=id,media_type,media_url,username,timestamp&access_token=" +
-            access_token;
-          xhrMedia.addEventListener("readystatechange", function() {
-            if (this.readyState === 4) {
-              that.setState({ allMediaData: JSON.parse(this.responseText) });
-            }
-          });
+  //   setTimeout(function() {
+  //     if (mediaId.size !== 0) {
+  //       mediaId.forEach((id) => {
+  //         // console.log(id);
+  //         urlMedia =
+  //           "https://graph.instagram.com/" +
+  //           id +
+  //           "?fields=id,media_type,media_url,username,timestamp&access_token=" +
+  //           access_token;
+  //         xhrMedia.addEventListener("readystatechange", function() {
+  //           if (this.readyState === 4) {
+  //             that.setState({ allMediaData: JSON.parse(this.responseText) });
+  //           }
+  //         });
 
-          xhrMedia.open("GET", urlMedia);
-          xhrMedia.setRequestHeader("Cache-Control", "no-cache");
-          xhrMedia.send(dataMedia);
-        });
-      }
-    }, 1000);
-  }
+  //         xhrMedia.open("GET", urlMedia);
+  //         xhrMedia.setRequestHeader("Cache-Control", "no-cache");
+  //         xhrMedia.send(dataMedia);
+  //       });
+  //     }
+  //   }, 1000);
+  // }
 
   render() {
     return (
       <div>
-        <Header />
+        <Header searchbox="true" showavatar="true" />
       </div>
     );
   }
