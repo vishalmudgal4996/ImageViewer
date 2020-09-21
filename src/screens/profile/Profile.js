2c0c06a30dd4421573fbd63cc15ca307b3c2fdba
vishalmudgal4996 <vishalmudgal4996@gmail.com>
2020-09-21 22:02:43 +0530

    profile page info-header designed

--- src/screens/profile/Profile.js
+++ src/screens/profile/Profile.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 import "./Profile.css";
 import Header from "../../common/header/Header";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import EditIcon from "@material-ui/icons/Edit";
+import Fab from "@material-ui/core/Fab";
 
 class Profile extends Component {
   constructor() {
@@ -11,6 +16,47 @@ class Profile extends Component {
     return (
       <div>
         <Header profileOptions="true" />
+        <div className="profile-container">
+          <div className="profile-info">
+            <div className="profile-header">
+              <div className="profile-header-details">
+                <div className="profile-icon">
+                  <IconButton
+                    aria-label="account of current user"
+                    aria-controls="customized-menu"
+                    aria-haspopup="true"
+                    color="inherit"
+                  >
+                    <AccountCircle style={{ fontSize: 150 }} />
+                  </IconButton>
+                </div>
+                <div className="profile-details">
+                  <div className="account-username">
+                    <Typography variant="h6">Username</Typography>
+                  </div>
+                  <div className="account-details">
+                    <Typography variant="subtitle1">Posts: 6</Typography>
+                    <Typography variant="subtitle1">Follows: 4</Typography>
+                    <Typography variant="subtitle1">Followed By: 6</Typography>
+                  </div>
+                  <div className="account-footer">
+                    <div className="footer-title">
+                      <Typography variant="h5">UpGrad Education</Typography>
+                    </div>
+                    <Fab
+                      color="secondary"
+                      aria-label="edit"
+                      onClick={this.openEditModalHandler}
+                    >
+                      <EditIcon />
+                    </Fab>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="profile-body">body</div>
+          </div>
+        </div>
       </div>
     );
   }
