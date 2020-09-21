2522d15c0593227db8a7ef0b42fac20aa69113c8
vishalmudgal4996 <vishalmudgal4996@gmail.com>
2020-09-21 17:43:24 +0530

    profile page setup

---
+++ src/screens/profile/Profile.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./Profile.css";
+
+class Profile extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return <div>profile page</div>;
+  }
+}
+
+export default Profile;
